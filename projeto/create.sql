2898a9203e99966edc13d7d845172f68b64898ab
vitor-amartins <vmam@cin.ufpe.br>
2018-04-08 17:12:12 -0300

    First commit. Creating and Inserting

---
+++ projeto/create.sql
@@ -0,0 +1,125 @@
+DROP TABLE Curso;
+DROP TABLE Bacharelado;
+DROP TABLE Licenciatura;
+DROP TABLE Endereco;
+DROP TABLE Aluno;
+DROP TABLE Professor;
+DROP TABLE Email;
+DROP TABLE Disciplina;
+DROP TABLE Sala;
+DROP TABLE Ic;
+DROP TABLE Projeto;
+DROP TABLE Cursa;
+DROP TABLE Aula;
+
+-------------------------
+
+CREATE TABLE Curso (
+	codigo NUMBER,
+	nome VARCHAR2(30) NOT NULL,
+	nota_mec NUMBER(4,2),
+	CONSTRAINT curso_pk PRIMARY KEY (codigo),
+	CONSTRAINT curso_ck CHECK (nota_mec >= 0) AND (nota_mec <= 10)
+);
+
+CREATE TABLE Bacharelado (
+	codigo NUMBER,
+	linha_de_pesquisa VARCHAR2(30),
+	CONSTRAINT bacharelado_pk PRIMARY KEY (codigo),
+	CONSTRAINT bacharelado_fk FOREIGN KEY (codigo) REFERENCES Curso(codigo)
+);
+
+CREATE TABLE Licenciatura (
+	codigo NUMBER,
+	disciplinas_obrigatorias VARCHAR2(30),
+	CONSTRAINT licenciatura_pk PRIMARY KEY (codigo),
+	CONSTRAINT licenciatura_fk FOREIGN KEY (codigo) REFERENCES Curso(codigo)
+);
+
+CREATE TABLE Endereco (
+	cep NUMBER,
+	descricao VARCHAR2(30),
+	CONSTRAINT endereco_pk PRIMARY KEY (cep)
+);
+
+CREATE TABLE Aluno (
+	cpf_aluno NUMBER,
+	data_nasc DATE NOT NULL,
+	codigo_curso NUMBER NOT NULL,
+	cpf_padrinho NUMBER,			-- Cannot be not null, because of the insert
+	nome VARCHAR2(30) NOT NULL,
+	cep NUMBER NOT NULL,
+	CONSTRAINT aluno_pk PRIMARY KEY (cpf_aluno),
+	CONSTRAINT aluno_fk1 FOREIGN KEY (cpf_padrinho) REFERENCES Aluno(cpf_aluno),
+	CONSTRAINT aluno_fk2 FOREIGN KEY (codigo_curso) REFERENCES Curso(codigo),
+	CONSTRAINT aluno_fk2 FOREIGN KEY (cep) REFERENCES Endereco(cep)
+);
+
+CREATE TABLE Professor (
+	cpf_professor NUMBER,
+	data_nasc DATE NOT NULL,
+	nome VARCHAR2(30) NOT NULL,
+	CONSTRAINT professor_pk PRIMARY KEY (cpf_professor)
+);
+
+CREATE TABLE Email (
+	email VARCHAR2(30),
+	cpf_professor NUMBER,
+	CONSTRAINT email_pk PRIMARY KEY (email, cpf_professor),
+	CONSTRAINT email_fk FOREIGN KEY (cpf_professor) REFERENCES Professor(cpf_professor)
+);
+
+CREATE TABLE Disciplina (
+	codigo_disc NUMBER,
+	qnt_alunos NUMBER NOT NULL,
+	cpf_professor NUMBER NOT NULL,
+	nome VARCHAR2(30)  NOT NULL,
+	CONSTRAINT disciplina_pk PRIMARY KEY (codigo_disc),
+	CONSTRAINT disciplina_fk FOREIGN KEY (cpf_professor) REFERENCES Professor(cpf_professor),
+);
+
+CREATE TABLE Sala (
+	codigo NUMBER,
+	capacidade NUMBER NOT NULL,
+	centro VARCHAR2(30) NOT NULL,
+	CONSTRAINT sala_pk PRIMARY KEY (codigo)
+);
+
+CREATE TABLE Ic (
+	codigo NUMBER,
+	data_inic DATE NOT NULL,
+	tema VARCHAR2(100) NOT NULL,
+	cpf_aluno NUMBER UNIQUE NOT NULL,
+	CONSTRAINT ic_pk PRIMARY KEY (codigo),
+	CONSTRAINT ic_fk FOREIGN KEY (cpf_aluno) REFERENCES Aluno(cpf_aluno)
+);
+
+CREATE TABLE Projeto (
+	cpf_professor NUMBER,
+	titulo VARCHAR2(100) NOT NULL,
+	descricao VARCHAR2(200) NOT NULL,
+	investimento NUMBER(10,2) NOT NULL,
+	CONSTRAINT projeto_pk PRIMARY KEY (cpf_professor),
+	CONSTRAINT projeto_fk FOREIGN KEY (cpf_professor) REFERENCES Professor(cpf_professor)
+);
+
+CREATE TABLE Cursa (
+	cpf_aluno NUMBER,
+	codigo_disc NUMBER,
+	media NUMBER(4,2) NOT NULL,
+	CONSTRAINT cursa_pk PRIMARY KEY (cpf_aluno, codigo_disc),
+	CONSTRAINT cursa_fk1 FOREIGN KEY (cpf_aluno) REFERENCES Aluno(cpf_aluno),
+	CONSTRAINT cursa_fk2 FOREIGN KEY (codigo_disc) REFERENCES Disciplina(codigo_disc),
+	CONSTRAINT cursa_ck CHECK (media >= 0) AND (media <= 10)
+);
+
+CREATE TABLE Aula (
+	codigo_sala NUMBER,
+	cpf_aluno NUMBER,
+	cpf_professor NUMBER,
+	horario SYSDATE,
+	CONSTRAINT aula_pk PRIMARY KEY (codigo_sala, cpf_aluno, cpf_professor, horario),
+	CONSTRAINT aula_fk1 FOREIGN KEY (codigo_sala) REFERENCES Sala(codigo),
+	CONSTRAINT aula_fk2 FOREIGN KEY (cpf_aluno) REFERENCES Aluno(cpf_aluno),
+	CONSTRAINT aula_fk3 FOREIGN KEY (cpf_professor) REFERENCES Professor(cpf_professor)
+);
---
+++ projeto/insert.sql
@@ -0,0 +1,66 @@
+INSERT INTO Curso(codigo, nome, nota_mec) VALUES (001, 'Engenharia da Computação', 10.00);
+INSERT INTO Curso(codigo, nome, nota_mec) VALUES (002, 'Letras', 8.50);
+INSERT INTO Curso(codigo, nome, nota_mec) VALUES (003, 'Matemática', 9.52);
+INSERT INTO Curso(codigo, nome, nota_mec) VALUES (004, 'Ciência Política', 7.60);
+INSERT INTO Curso(codigo, nome, nota_mec) VALUES (005, 'Ciência da Computação', 6.20);
+INSERT INTO Curso(codigo, nome, nota_mec) VALUES (006, 'Arquitetura', 8.24);
+
+INSERT INTO Bacharelado(codigo, linha_de_pesquisa) VALUES (001, 'Informática');
+INSERT INTO Bacharelado(codigo, linha_de_pesquisa) VALUES (004, 'Política');
+INSERT INTO Bacharelado(codigo, linha_de_pesquisa) VALUES (005, 'Informática');
+INSERT INTO Bacharelado(codigo, linha_de_pesquisa) VALUES (006, 'Urbanismo');
+
+INSERT INTO Licenciatura(codigo, disciplinas_obrigatorias) VALUES (002, 'Metodologia');
+INSERT INTO Licenciatura(codigo, disciplinas_obrigatorias) VALUES (003, 'Pedagogia');
+
+INSERT INTO Endereco(cep, descricao) VALUES (00000, 'Rua dos Bobos');
+INSERT INTO Endereco(cep, descricao) VALUES (00020, 'Rua dos Caras Dahoras');
+INSERT INTO Endereco(cep, descricao) VALUES (00040, 'Rua de Cima');
+INSERT INTO Endereco(cep, descricao) VALUES (00060, 'Rua de Baixo');
+INSERT INTO Endereco(cep, descricao) VALUES (00080, 'Rua Esburacada');
+INSERT INTO Endereco(cep, descricao) VALUES (57020, 'Rua Formosa');
+
+INSERT INTO Aluno(cpf_aluno, data_nasc, codigo_curso, cpf_padrinho, nome, cep) VALUES (0000, to_date('19980407', 'yyyymmdd'), 001, NULL, 'Vitor', 00020);
+INSERT INTO Aluno(cpf_aluno, data_nasc, codigo_curso, cpf_padrinho, nome, cep) VALUES (1111, to_date('19980326', 'yyyymmdd'), 006, 0000, 'Maria', 57020);
+INSERT INTO Aluno(cpf_aluno, data_nasc, codigo_curso, cpf_padrinho, nome, cep) VALUES (2222, to_date('19980607', 'yyyymmdd'), 003, 1111, 'Lucas', 00080);
+INSERT INTO Aluno(cpf_aluno, data_nasc, codigo_curso, cpf_padrinho, nome, cep) VALUES (3333, to_date('19980825', 'yyyymmdd'), 002, 2222, 'Allex', 00040);
+INSERT INTO Aluno(cpf_aluno, data_nasc, codigo_curso, cpf_padrinho, nome, cep) VALUES (4444, to_date('19981113', 'yyyymmdd'), 005, 1111, 'Karol', 00000);
+INSERT INTO Aluno(cpf_aluno, data_nasc, codigo_curso, cpf_padrinho, nome, cep) VALUES (5555, to_date('19990102', 'yyyymmdd'), 004, 0000, 'Sammy', 00060);
+
+INSERT INTO Professor(cpf_professor, data_nasc, nome) VALUES (0101, to_date('19900229', 'yyyymmdd'), 'Roberval');
+INSERT INTO Professor(cpf_professor, data_nasc, nome) VALUES (1212, to_date('19870321', 'yyyymmdd'), 'Cresovil');
+INSERT INTO Professor(cpf_professor, data_nasc, nome) VALUES (2323, to_date('19701219', 'yyyymmdd'), 'Josueldo');
+
+INSERT INTO Email(email, cpf_professor) VALUES ('roberval@sexy.com', 0101);
+INSERT INTO Email(email, cpf_professor) VALUES ('cresovil@sexy.com', 1212);
+INSERT INTO Email(email, cpf_professor) VALUES ('ovil1234@sexy.com', 1212);
+INSERT INTO Email(email, cpf_professor) VALUES ('josueldo@sexy.com', 2323);
+
+INSERT INTO Disciplina(codigo_disc, qnt_alunos, cpf_professor, nome) VALUES (1, 0, 0101, 'Programação');
+INSERT INTO Disciplina(codigo_disc, qnt_alunos, cpf_professor, nome) VALUES (2, 0, 1212, 'História da Arquitetura');
+INSERT INTO Disciplina(codigo_disc, qnt_alunos, cpf_professor, nome) VALUES (3, 0, 2323, 'Cálculo I');
+INSERT INTO Disciplina(codigo_disc, qnt_alunos, cpf_professor, nome) VALUES (4, 0, 2323, 'Cálculo II');
+INSERT INTO Disciplina(codigo_disc, qnt_alunos, cpf_professor, nome) VALUES (5, 0, 2323, 'Cálculo III');
+INSERT INTO Disciplina(codigo_disc, qnt_alunos, cpf_professor, nome) VALUES (6, 0, 2323, 'Física I');
+INSERT INTO Disciplina(codigo_disc, qnt_alunos, cpf_professor, nome) VALUES (7, 0, 2323, 'Física II');
+INSERT INTO Disciplina(codigo_disc, qnt_alunos, cpf_professor, nome) VALUES (8, 0, 2323, 'Física III');
+
+INSERT INTO Sala(codigo, capacidade, centro) VALUES (002, 050, 'CCEN');
+INSERT INTO Sala(codigo, capacidade, centro) VALUES (003, 060, 'CCEN');
+INSERT INTO Sala(codigo, capacidade, centro) VALUES (004, 070, 'CCEN');
+INSERT INTO Sala(codigo, capacidade, centro) VALUES (005, 090, 'CCEN');
+INSERT INTO Sala(codigo, capacidade, centro) VALUES (013, 120, 'Área 2');
+INSERT INTO Sala(codigo, capacidade, centro) VALUES (015, 110, 'Área 2');
+INSERT INTO Sala(codigo, capacidade, centro) VALUES (112, 050, 'CIN');
+
+INSERT INTO Ic(codigo, data_inic, tema, cpf_aluno) VALUES (01, to_date('20180219'), 'IoT', 0000);
+INSERT INTO Ic(codigo, data_inic, tema, cpf_aluno) VALUES (02, to_date('20180219'), 'TNA', 1111);
+INSERT INTO Ic(codigo, data_inic, tema, cpf_aluno) VALUES (03, to_date('20180219'), 'PSB', 5555);
+
+INSERT INTO Projeto(cpf_professor, titulo, descricao, investimento) VALUES (0101, 'LMS', 'Learning Management Systems', 200000.00);
+INSERT INTO Projeto(cpf_professor, titulo, descricao, investimento) VALUES (1212, 'WTF', 'Wake Taller Faster', 150000.00);
+
+INSERT INTO Cursa(cpf_aluno, codigo_disc, media) VALUES (0000, 1, 9.85);
+INSERT INTO Cursa(cpf_aluno, codigo_disc, media) VALUES (2222, 5, 8.50);
+
+INSERT INTO Aula(codigo_sala, cpf_aluno, cpf_professor, horario) VALUES (013, 2222, 2323, SYSDATE());
